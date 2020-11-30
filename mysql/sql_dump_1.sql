330691a10d8290491337a81722ac148be30220ed
Jannik Merz <jm136@hdm-stuttgart.de>
2020-11-30 18:23:56 +0100

    sql: fremdschlüssel erweitert

--- mysql/sql_dump_1.sql
+++ mysql/sql_dump_1.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE  IF NOT EXISTS `electivApp` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `electivApp`;
 
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 
 -- -----------------------------------------------------
 -- Table `electivapp`.`personen`
@@ -50,10 +50,38 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `electivapp`.`teilnahmen` (
   `id` INT NOT NULL DEFAULT '0',
   `lehrangebot` INT NOT NULL,
+  `teilnehmer` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_teilnahmen_projekte_idx` (`lehrangebot` ASC) VISIBLE,
+  INDEX `fk_teilnahmen_personen1_idx` (`teilnehmer` ASC) VISIBLE,
   CONSTRAINT `fk_teilnahmen_projekte`
     FOREIGN KEY (`lehrangebot`)
+    REFERENCES `electivapp`.`projekte` (`id`),
+  CONSTRAINT `fk_teilnahmen_personen1`
+    FOREIGN KEY (`teilnehmer`)
+    REFERENCES `electivapp`.`personen` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `electivapp`.`personen_hat_projekte`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `electivapp`.`personen_hat_projekte` (
+  `dozent` INT NOT NULL,
+  `veranstaltung` INT NOT NULL,
+  PRIMARY KEY (`dozent`, `veranstaltung`),
+  INDEX `fk_personen_has_projekte_projekte1_idx` (`veranstaltung` ASC) VISIBLE,
+  INDEX `fk_personen_has_projekte_personen1_idx` (`dozent` ASC) VISIBLE,
+  CONSTRAINT `fk_personen_has_projekte_personen1`
+    FOREIGN KEY (`dozent`)
+    REFERENCES `electivapp`.`personen` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_personen_has_projekte_projekte1`
+    FOREIGN KEY (`veranstaltung`)
     REFERENCES `electivapp`.`projekte` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
