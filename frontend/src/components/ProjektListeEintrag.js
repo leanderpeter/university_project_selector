7d20e197c39f732043bcbe4cd6c20a160514db05
Alexander Hofstetter <ah214@hdm-stuttgart.de>
2020-12-02 15:51:35 +0100

    Update ProjektListeEintrag.js
    erste schritte für projekte teilnehmen

--- frontend/src/components/ProjektListeEintrag.js
+++ frontend/src/components/ProjektListeEintrag.js
@@ -72,18 +72,8 @@ class ProjektListeEintrag extends Component {
 
 	teilnahmeButtonClicked = event => {
     	//Logik fuer Teilnahme Button
-    	    var count = 1;
-	    	function setColor(btn, color) {
-		        var property = document.getElementById(btn);
-		        if (count == 0) {
-		            property.style.backgroundColor = "#FFFFFF"
-		            count = 1;        
-		        }
-		        else {
-		            property.style.backgroundColor = "#7FFF00"
-		            count = 0;
-		        }
-    }
+    	console.log("Teilnehmen!!");
+    	console.log(this.props.projekt.id);
 	}
 
 	/** Renders the component */
