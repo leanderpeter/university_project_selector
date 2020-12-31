ac65479c3a494d8c0775b4c6e580ce35b1842cc5
Daria-Bilyk <db134@hdm-stuttgart.de>
2020-12-31 20:18:48 +0100

    Notenliste angepasst, noch nicht fertig

--- frontend/src/components/Notenlisten.js
+++ frontend/src/components/Notenlisten.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, TextField, InputAdornment, IconButton, Grid, Typography, MobileStepper } from '@material-ui/core';
+import { withStyles, TextField, InputAdornment, IconButton, Grid, Typography } from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear'
 import { withRouter } from 'react-router-dom';
 import { ElectivAPI } from '../api';
@@ -8,10 +8,34 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import ContextErrorMessage from './dialogs/ContextErrorMessage';
 import LoadingProgress from './dialogs/LoadingProgress';
-import NotenlistenEintrag from './NotenlistenEintrag';
 
+import EdvListeEintrag from './EdvListeEintrag';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.darkgrey,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(4n+1)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
 class Notenlisten extends Component {
 
@@ -28,6 +52,7 @@ class Notenlisten extends Component {
 		this.state = {
       module: [],
       semester: [],
+      teilnahmen : [],
       filteredModule: [],
       semesterwahl: null,
 			edvFilter: '',
@@ -80,14 +105,30 @@ class Notenlisten extends Component {
       });
   }
 
-  handleChange = (semester) => {
+  handleSemesterChange = (semester) => {
     this.setState({
       semesterwahl: semester.target.value,
       expandedModulID: null
     })
     setTimeout(() => {
-      console.log('Ausgewählte Semester ID:',this.state.semesterwahl)
-    }, 500);
+      console.log('Ausgewählte Semester ID:',this.state.semesterwahl)   
+    if (this.state.modulwahl != null) {
+      this.getTeilnahmen_by_modul_und_semester();
+    }
+    }, 0);
+  };
+
+  handleModulChange = (modul) => {
+    this.setState({
+      modulwahl: modul.target.value,
+      expandedModulID: null
+    })
+    setTimeout(() => {
+      console.log('Ausgewählte Modul ID:',this.state.modulwahl)   
+    if (this.state.semesterwahl != null) {
+      this.getTeilnahmen_by_modul_und_semester();
+    }
+    }, 0);
   };
 
 	// Lifecycle methode, wird aufgerufen wenn componente in den DOM eingesetzt wird
@@ -106,6 +147,27 @@ class Notenlisten extends Component {
       expandedModulID: newID,
     });
   }
+
+    // API Anbindung um Teilnahmen des Students vom Backend zu bekommen 
+    getTeilnahmen_by_modul_und_semester = () => {
+      ElectivAPI.getAPI().getTeilnahmen_by_modul_und_semester(this.state.modulwahl, this.state.semesterwahl)
+      .then(teilnahmeBOs =>
+          this.setState({
+              teilnahmen: teilnahmeBOs,
+              error: null,
+              loadingInProgress: false,
+          })).catch(e =>
+              this.setState({
+                  teilnahmen: [],
+                  error: e,
+                  loadingInProgress: false,
+              }));
+      this.setState({
+          error: null,
+          loadingInProgress: true,
+          loadingTeilnahmeError: null
+      });
+    }
 
 
   filterFieldValueChange = event => {
@@ -132,7 +194,7 @@ class Notenlisten extends Component {
 	render() {
     
     const { classes } = this.props;
-    const { module, semester, semesterwahl, filteredModule, edvFilter,  expandedModulID, loadingInProgress, error } = this.state;
+    const { module, semester, semesterwahl, modulwahl, teilnahmen, filteredModule, edvFilter,  expandedModulID, loadingInProgress, error } = this.state;
     return (
     <div className={classes.root}>
         <Grid className={classes.header} container spacing={1} justify='flex-start' alignItems='space-between'>
@@ -141,7 +203,7 @@ class Notenlisten extends Component {
             semester ?
             <FormControl className={classes.formControl}>
               <InputLabel>Semester</InputLabel> 
-                <Select value = {semester.id} onChange={this.handleChange}>
+                <Select value = {semester.id} onChange={this.handleSemesterChange}>
                   {
                   semester.map(semester =>
                   <MenuItem value={semester.getID()}><em>{semester.getname()}</em></MenuItem>
@@ -158,7 +220,26 @@ class Notenlisten extends Component {
             </FormControl>
           }
           </Grid>
-          <Grid item xs></Grid>
+          <Grid item xs>
+          { module ?
+            <FormControl className={classes.formControl}>
+              <InputLabel>Modul</InputLabel> 
+                <Select onChange={this.handleModulChange}>
+                  { module.map(modul =>
+                    <MenuItem value={modul.getID()}><em>{modul.getname()}</em></MenuItem>
+                    )
+                  }
+                </Select>
+            </FormControl>
+            :
+            <FormControl className={classes.formControl}>
+              <InputLabel>Modul</InputLabel>
+               <Select value="">
+                 <MenuItem value=""><em>Module noch nicht geladen</em></MenuItem>
+                </Select>
+            </FormControl>
+          }
+          </Grid>
           <Grid item className={classes.filter}>
               <Typography>
               Filter Notenlisten nach EDV-Nummer:
@@ -181,11 +262,39 @@ class Notenlisten extends Component {
               />
           </Grid>
         </Grid>
-        {
-          filteredModule.map(modul =>
-          <NotenlistenEintrag key={modul.getID()} modul = {modul} semesterwahl = {semesterwahl} expandedState={expandedModulID === modul.getID()} onExpandedStateChange={this.onExpandedStateChange}/>
-          )
-        }
+        { semesterwahl && modulwahl ?
+            <TableContainer>
+                  <Table className={classes.table} aria-label="customized table">
+                      <TableHead>
+                          <StyledTableRow>
+                              <StyledTableCell align="left">Student</StyledTableCell>
+                              <StyledTableCell align="left">Matrikelnummer</StyledTableCell>
+                              <StyledTableCell align="center">Note</StyledTableCell>
+                          </StyledTableRow>
+                      </TableHead>
+                      <TableBody>
+                        {
+                                teilnahmen ?
+                                <>
+                                {
+                                    teilnahmen.map(teilnahme => 
+                                        <EdvListeEintrag key={teilnahme.getID()} teilnahme = {teilnahme}
+                                        onExpandedStateChange={this.onExpandedStateChange}
+                                        show={this.props.show}
+                                    />) 
+                                }
+                                </>
+                                :
+                                <></>
+                            }
+                      </TableBody>
+                    </Table>
+              </TableContainer>
+            :
+            <Grid container justify-content='center 'alignItems='center'>
+              <Grid item xs><Typography variant='body1' className={classes.warnung}> Bitte wählen Sie zunächst ein Semester aus</Typography></Grid>
+            </Grid>
+            }
         <LoadingProgress show={loadingInProgress} />
         <ContextErrorMessage error={error} contextErrorMsg={`The list of Projects could not be loaded.`} onReload={this.getProjekte} />
     </div>
