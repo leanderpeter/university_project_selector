f571a162ec77070688e064fdc5585c62f9a61678
Raphael Mueller <rm056@hdm-stuttgart.de>
2021-01-18 20:30:43 +0100

    Code anpassungen und Formatierung

--- frontend/src/components/ProjektDozentListeEintrag.js
+++ frontend/src/components/ProjektDozentListeEintrag.js
@@ -20,55 +20,55 @@ import AccountList from './AccountList';
 
 class ProjektDozentListeEintrag extends Component {
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		// Status initalisieren
-		this.state = {
-			projekt: props.projekt,
+    // Status initalisieren
+    this.state = {
+      projekt: props.projekt,
       projektarten: [],
-			showProjektForm: false,
+      showProjektForm: false,
       showProjektDeleteDialog: false,
-		};
-	}
+    };
+  }
 
-	// Handles events wenn sich der status der oeffnung aendert
-	expansionPanelStateChanged = () => {
-		this.props.onExpandedStateChange(this.props.projekt);
+  // Handles events wenn sich der status der oeffnung aendert
+  expansionPanelStateChanged = () => {
+    this.props.onExpandedStateChange(this.props.projekt);
 
     /*
-		// Teilnahme Button deaktivieren, sofern Teilnehmer bereits in Projekt eingeschrieben
-		if( this.props.projekt.teilnehmerListe.indexOf(this.props.currentStudent.id)> -1){
-		    this.setState({teilnahmeButtonDisabled:true});
-		}
+    // Teilnahme Button deaktivieren, sofern Teilnehmer bereits in Projekt eingeschrieben
+    if( this.props.projekt.teilnehmerListe.indexOf(this.props.currentStudent.id)> -1){
+        this.setState({teilnahmeButtonDisabled:true});
+    }
     */
   }
-  
+
   getProjekte = () => {
     this.props.getProjekte();
   }
 
   projektFormClosed = (projekt) => {
-    if (projekt){
+    if (projekt) {
       this.setState({
         projekt: projekt,
         showProjektForm: false
       });
-    }else {
+    } else {
       this.setState({
         showProjektForm: false
       });
     }
   }
 
-	bearbeitenButtonClicked = event => {
+  bearbeitenButtonClicked = event => {
     event.stopPropagation();
     this.setState({
       showProjektForm: true
     });
   }
-  
-  projektDeleteButtonClicked =  event => {
+
+  projektDeleteButtonClicked = event => {
     event.stopPropagation();
     this.setState({
       showProjektDeleteDialog: true
@@ -76,19 +76,19 @@ class ProjektDozentListeEintrag extends Component {
   }
 
   projektDeleteClosed = () => {
-      this.setState({
-        showProjektDeleteDialog: false
-      });
+    this.setState({
+      showProjektDeleteDialog: false
+    });
   }
 
   getProjektart = () => {
     ElectivAPI.getAPI().getProjektart().then(projektartBOs =>
       this.setState({
         projektarten: projektartBOs
-      })).catch(e => 
-    this.setState({
-      //projektarten: []
-    }));
+      })).catch(e =>
+        this.setState({
+          //projektarten: []
+        }));
   }
 
   componentDidMount() {
@@ -96,11 +96,11 @@ class ProjektDozentListeEintrag extends Component {
   }
 
 
-	/** Renders the component */
+  /** Renders the component */
   render() {
-    const { classes, expandedState} = this.props;
+    const { classes, expandedState } = this.props;
     // Use the states projekt
-    const { projekt, projektarten, showProjektForm, showProjektDeleteDialog} = this.state;
+    const { projekt, projektarten, showProjektForm, showProjektDeleteDialog } = this.state;
 
     // console.log(this.state);
     return (
@@ -117,53 +117,53 @@ class ProjektDozentListeEintrag extends Component {
               </Grid>
               <Grid item xs />
               <Grid item>
-              	<Typography variant='body2' color={'textSecondary'}>Details</Typography>
-            	</Grid>
+                <Typography variant='body2' color={'textSecondary'}>Details</Typography>
+              </Grid>
             </Grid>
           </AccordionSummary>
           <AccordionDetails>
             <Typography variant='body1' color={'textSecondary'}>
-                <b>Beschreibung: </b> {projekt.getbeschreibung()} <br />
-                <b>Raum: </b>{projekt.getraum()}<br />
-                <b>Maximale Teilnehmer: </b>{projekt.getmax_teilnehmer()}<br />
-                <b>Betreuer: </b>{projekt.getbetreuer()}<br />
-                <b>Externer Partner: </b>{projekt.getexterner_partner()}<br />
-                <b>Wöchentlich: </b>{projekt.getwoechentlich() == "1" ? "Ja" : "Nein"}<br />
-                <b>Blocktage vor Prüfungsphase: </b>{projekt.getanzahl_block_vor()}<br />
-                <b>Blocktage während Prüfungsphase: </b>{projekt.getanzahl_block_in()}<br />
-                <b>Sprache: </b>{projekt.getsprache()}<br />
-                {projektarten.length > 0 && projekt ? 
+              <b>Beschreibung: </b> {projekt.getbeschreibung()} <br />
+              <b>Raum: </b>{projekt.getraum()}<br />
+              <b>Maximale Teilnehmer: </b>{projekt.getmax_teilnehmer()}<br />
+              <b>Betreuer: </b>{projekt.getbetreuer()}<br />
+              <b>Externer Partner: </b>{projekt.getexterner_partner()}<br />
+              <b>Wöchentlich: </b>{projekt.getwoechentlich() == "1" ? "Ja" : "Nein"}<br />
+              <b>Blocktage vor Prüfungsphase: </b>{projekt.getanzahl_block_vor()}<br />
+              <b>Blocktage während Prüfungsphase: </b>{projekt.getanzahl_block_in()}<br />
+              <b>Sprache: </b>{projekt.getsprache()}<br />
+              {projektarten.length > 0 && projekt ?
                 <>
-                <b>Projektart: </b>{projektarten[projekt.art-1].name}<br />
-                <b>SWS: </b>{projektarten[projekt.art-1].sws}<br />
-                <b>ECTS: </b>{projektarten[projekt.art-1].ects}<br />
+                  <b>Projektart: </b>{projektarten[projekt.art - 1].name}<br />
+                  <b>SWS: </b>{projektarten[projekt.art - 1].sws}<br />
+                  <b>ECTS: </b>{projektarten[projekt.art - 1].ects}<br />
                 </>
                 :
                 <>
-                <b>ECTS noch nicht geladen</b><br />
+                  <b>ECTS noch nicht geladen</b><br />
                 </>
-                }
-                <b>Präferierter Block: </b>{projekt.getpraeferierte_block()}<br />
+              }
+              <b>Präferierter Block: </b>{projekt.getpraeferierte_block()}<br />
 
             </Typography>
           </AccordionDetails>
           <AccordionDetails>
-          <Grid container justify="flex-end" alignItems="center" spacing={2}>
-            <Grid item>
-              <Tooltip title='Löschen' placement="left">
-                <IconButton className={classes.projektDeleteButton}  variant="contained"  onClick={this.projektDeleteButtonClicked}><DeleteIcon /></IconButton>
-              </Tooltip>
-            </Grid>
-            <Grid item>
-              <Button id='btn' className={classes.bearbeitenButton} variant='contained' color='primary' onClick={this.bearbeitenButtonClicked}>
-              Bearbeiten
+            <Grid container justify="flex-end" alignItems="center" spacing={2}>
+              <Grid item>
+                <Tooltip title='Löschen' placement="left">
+                  <IconButton className={classes.projektDeleteButton} variant="contained" onClick={this.projektDeleteButtonClicked}><DeleteIcon /></IconButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Button id='btn' className={classes.bearbeitenButton} variant='contained' color='primary' onClick={this.bearbeitenButtonClicked}>
+                  Bearbeiten
               </Button>
+              </Grid>
             </Grid>
-          </Grid>
           </AccordionDetails>
         </Accordion>
-        <ProjektForm show={showProjektForm} projekt={projekt} onClose={this.projektFormClosed} getProjekte= {this.getProjekte}/>
-        <ProjektDelete show={showProjektDeleteDialog} projekt={projekt} onClose={this.projektDeleteClosed} getProjekte= {this.getProjekte}/>
+        <ProjektForm show={showProjektForm} projekt={projekt} onClose={this.projektFormClosed} getProjekte={this.getProjekte} />
+        <ProjektDelete show={showProjektDeleteDialog} projekt={projekt} onClose={this.projektDeleteClosed} getProjekte={this.getProjekte} />
       </div>
     );
   }
